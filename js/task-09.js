efd9e3dcbb5fc01beae60bffe1aa151a686a77cb
WiktorPospiech <wiktor.pospiech@gmail.com>
2023-08-05 19:18:24 +0200

    task-09 solution added

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changer = document.querySelector("button.change-color");
+const span = document.querySelector("span.color");
+const body = document.body;
+
+const changeBackground = () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  span.textContent = randomColor;
+};
+
+changer.addEventListener("click", changeBackground);
